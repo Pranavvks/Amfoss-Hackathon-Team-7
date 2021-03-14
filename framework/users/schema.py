e1c7c92902de4b889124d4bf156e4970e486adae
Nehal Nevle <nehalsuperdude@gmail.com>
2021-03-14 16:19:11 +0530

    tasks api

--- framework/users/schema.py
+++ framework/users/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType, ObjectType
-from users.models import User, Team
+from users.models import User, Team, Task
 
 
 class UserType(DjangoObjectType):
@@ -12,9 +12,16 @@ class TeamType(DjangoObjectType):
     class Meta:
         model = Team
 
+class TaskType(DjangoObjectType):
+    class Meta:
+        model = Task
+
 class AuthInput(graphene.InputObjectType):
     username = graphene.String()
     password = graphene.String()
+
+class TasksInput(graphene.InputObjectType):
+    username = graphene.String()
 
 class HasUser(graphene.Mutation):
     class Arguments:
@@ -31,16 +38,29 @@ class HasUser(graphene.Mutation):
         except Exception:
             ok = False
             return HasUser(ok=ok)
-        
+
+
         
 
 class Query(ObjectType):
     teams = graphene.List(TeamType)
+    tasks = graphene.List(TaskType)
 
     def resolve_teams(self, info, **kwargs):
         return Team.objects.all()
+    
+    def resolve_tasks(self, info, **kwargs):
+        username = kwargs.get('username')
+
+        if username is not None:
+            return Task.objects.get(assigned=username)
+
+        return None
+    
+
 
 class Mutation(graphene.ObjectType):
     has_user = HasUser.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)

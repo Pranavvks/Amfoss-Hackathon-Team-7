6576989a31f6de75e22bf5b550cf873d996a7671
Jayakrishnan Jayu <jayu2442@gmail.com>
2021-03-14 17:46:51 +0530

    Add cors header package

--- framework/framework/urls.py
+++ framework/framework/urls.py
@@ -21,5 +21,5 @@ from framework.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]

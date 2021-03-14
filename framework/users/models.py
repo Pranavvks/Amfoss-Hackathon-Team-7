3feab954ad387c8212ed09e45470c9696c2b59b7
Jayakrishnan Jayu <jayu2442@gmail.com>
2021-03-14 12:41:48 +0530

    Add user and team model

--- framework/users/models.py
+++ framework/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=30)
+    group = models.ForeignKey(
+        "Team", 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.username
+
+class Team(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    leader = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
+
+
+    def __str__(self):
+        return self.name
